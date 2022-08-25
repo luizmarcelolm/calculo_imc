4089d9f1cfaefae8dcea8c2c83d62e8fc97382cf
luizmarcelolm <luizmarcelolm@hotmail.com>
2022-08-24 22:56:34 -0300

    criação de inputs e calculo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import InputMask from "react-input-mask";
 
 function App() {
+  
+  const [form, setForm] = useState("");
+
+  //função que armazena informações digitadas
+ 
+  function handleChange(event) {
+   
+    setForm({ ...form, [event.target.name]: event.target.value });
+    console.log("form", form);
+  }
+
+  //função para enviar
+  function handleSubmit(event) {
+    event.preventDefault();
+   
+    if (form.altura !== "" && form.peso !== "") {
+      const imc = form.peso / (form.altura * form.altura);
+      console.log("imc", imc);
+      alert("Seu IMC é " + imc);
+    } else {
+      alert("Altura e peso devem ser digitados!!!");
+    }
+    //Desafio: implemente uma lógica para verificar o IMC, e junto com o Alert, mostrar a classificação do IMC
+    //Por exemplo: "Seu IMC é 30 e sua Classificação é Sobrepeso"
+    // if ("alguma coisa") {
+    //   "isso acontece"
+    // }
+    // else {
+    //   "outra coisa acontece"
+    // }
+
+    // // ou você pode precisar por mais de 2 condições, e usar o Else If
+    // if ("alguma coisa") {
+    //   "isso acontece"
+    // }
+    // else if ("outra coisa") {
+    //  "outra coisa acontece"
+    // }
+    // else {
+    //   "por fim isso acontece"
+    // }
+  }
+
+  //JSX, equivale ao nosso HTML e CSS
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>CALCULO DE IMC</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="container">
+        <div className="form">
+          <div className="containerInputLabel">
+            <div className="childenContainerInputLabel">
+              <label>Qual sua altura?</label>
+              <InputMask
+                placeholder="ALTURA"
+                onChange={handleChange}
+                name="altura"
+                mask="9.99"
+              />
+            </div>
+            <div className="childenContainerInputLabel">
+              <label>Qual seu peso?</label>
+              <InputMask
+                placeholder="PESO"
+                onChange={handleChange}
+                name="peso"
+              />
+            </div>
+          </div>
+          <div className="containerButton">
+            <button>CALCULAR IMC</button>
+          </div>
+        </div>
+      </div>
+    </form>
   );
 }
 
