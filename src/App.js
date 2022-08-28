c0744551615b3478be06b210a039b4f575d8b9d8
luiz <luizmarcelolm@hotmail.com>
2022-08-27 23:29:41 -0300

    alteração na validaçao dos inputs

--- src/App.js
+++ src/App.js
@@ -2,46 +2,53 @@ import "./App.css";
 import React, { useState } from "react";
 import InputMask from "react-input-mask";
 
+
+
 function App() {
 
-  
   const [form, setForm] = useState("");
 
-  
-
   //função que armazena informações digitadas
- 
   function handleChange(event) {
-
    
     setForm({ ...form, [event.target.name]: event.target.value });
-    console.log("form", form);
+
+    
   }
+
+ 
 
   //função para enviar
   function handleSubmit(event) {
-    event.preventDefault();
     
-   
-         if (form.peso !== "" && form.altura !== ""){
+        //validação dos input
+         if (form.peso > 0 && form.altura > 0 ){
             const imc = form.peso / (form.altura * form.altura);
-            alert("Seu IMC é " + imc.toFixed(1));
-          } else {
-              return alert("Altura e peso devem ser digitadas!!!");
-      
-            }
+            return alert("Seu IMC é " + imc.toFixed(1));
+                        
+          }
+          else if (form.peso === "" && form.altura === ""){
+              event.preventDefault();
+
+              return alert("PREENCHA OS CAMPOS CORRETAMENTE !!!");
+           }
+            else{
+               event.preventDefault();
+               return alert("PREENCHA OS CAMPOS CORRETAMENTE !!!");
+                                        
+            }          
   }
- 
-  
-  
+
+  //formulário da aplicação
   return (
     <form onSubmit={handleSubmit}>
       <div className="container">
          <div className="form">
+         <h1>CÁLCULO IMC</h1>
           <div className="containerInputLabel">
             <div className="childenContainerInputLabel">
-              <label>Qual sua altura?</label>
-              <InputMask
+              <label for="altura">Qual sua altura?</label>
+              <InputMask 
                 placeholder="ALTURA"
                 onChange={handleChange}
                 name="altura" 
